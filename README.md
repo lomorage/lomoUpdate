b82039dd64803b0806578a03e88a1f93ab9c7af5
Elliot Liu (liuliu) <liuliu@cisco.com>
2019-10-02 20:55:49 -0700

    for windows building

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # lomoUpdate
 
 Used for lomoUpdate release, including Windows and OSX version
+
+# how to build on windows
+google choco and install choco
+run PS as admin: in PS shell: 
+
+> choco install zip
+
+then run on git-bash shell:
+
+> ./build-osx.sh win
--- build-osx.sh
+++ build-osx.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+PLATFORM=$1
+if [ "$PLATFORM" == "win" ]; then
+	go get github.com/Sirupsen/logrus
+	go get github.com/pkg/errors
+	go get github.com/urfave/cli
+fi
+
+
+
 nowDate=$(date +"%Y_%m_%d")
 nowTime=$(date +"%H_%M_%S")
 commitHash=$(git rev-parse --short HEAD)
@@ -12,7 +21,13 @@ sed -i ".bak" -E "s/[[:digit:]]{4}_[[:digit:]]{2}_[[:digit:]]{2}\.[[:digit:]]{2}
 versionNew=$(grep "const LomoUpdateVersion" main.go)
 echo "new verion: $versionNew"
 
-go build -o lomoupg
+if [ "$PLATFORM" == "win" ]; then
+	go build -o lomoupg.exe
+	zip -r lomoUpdateWin.zip lomoupg.exe
+else
+	go build -o lomoupg
+	zip -r lomoUpdateOSX.zip lomoupg
+	shasum -a256 lomoUpdateOSX.zip
+fi
 
-zip -r lomoUpdateOSX.zip lomoupg
-shasum -a256 lomoUpdateOSX.zip
+
